9a881fd91a3768b8c3adef35a1ae84cd15d7cea8
abromo79 <saidhamad3437@gmail.com>
2026-02-05 12:32:07 +0300

    Update greeting message in home route

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from PaaS Practical Session!"
+    return "Hello Abromo from PaaS Practical Session!"
 
 if __name__ == "__main__":
     app.run()
+
